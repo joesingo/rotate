c3b15b0d328bfd6858f455f114086f086f59b0e9
Joe Singleton <joesingo@gmail.com>
2018-01-03 20:33:05 +0000

    Allow a global variable AUDIO_PREFIX to be pre-pended to audio URLs
    For including game inside an existing web page, which would break
    relative links

--- rotate/js/audio.js
+++ rotate/js/audio.js
@@ -1,9 +1,20 @@
 function AudioManager() {
     this.muted = false;
     this.sounds = {};
+    this.loadSounds();
+}
+
+/*
+ * Populate `this.sounds` with Audio objects
+ */
+AudioManager.prototype.loadSounds = function() {
+    // Allow a variable AUDIO_PREFIX to be added to the start URLs pointing to
+    // audio files, in case game needs to be deployed into an existing web page
+    // in which case a relative link won't work
+    prefix = (typeof(AUDIO_PREFIX) !== "undefined" ? AUDIO_PREFIX : "");
 
     for (var name in AUDIO) {
-        this.sounds[name] = new Audio(AUDIO[name].path);
+        this.sounds[name] = new Audio(prefix + AUDIO[name].path);
         this.sounds[name].volume = AUDIO[name].volume || 1;
     }
 }
@@ -22,8 +33,6 @@ AudioManager.prototype.play = function(name) {
 
 function easteregg() {
     console.log("hehe");
-    for (var name in AUDIO) {
-        game.audio.sounds[name].src = "http://files.joesingo.co.uk/81109f59ff7f12bed867d0735052bdf3/" +
-                                       AUDIO[name].path;
-    }
+    AUDIO_PREFIX = "http://files.joesingo.co.uk/81109f59ff7f12bed867d0735052bdf3/";
+    game.audio.loadSounds();
 }
